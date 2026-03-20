0655eca8669816f48e3b7a8a2d4728d3a84b0b51
KhanithaHn <75299196+KhanithaHn@users.noreply.github.com>
2026-03-20 16:47:57 +0700

    Update firebase.js

--- assets/firebase.js
+++ assets/firebase.js
@@ -3,17 +3,41 @@
 <script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
 <script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
 
-<script>
+//<script>
   // TODO: ใส่ค่าจาก Firebase Console ของโปรเจ็กต์คุณ
+  //const firebaseConfig = {
+   // apiKey: "YOUR_KEY",
+   // authDomain: "YOUR_PROJECT.firebaseapp.com",
+    //projectId: "YOUR_PROJECT",
+   // storageBucket: "YOUR_PROJECT.appspot.com",
+   // messagingSenderId: "YOUR_SENDER_ID",
+   // appId: "YOUR_APP_ID"
+  //};
+ // firebase.initializeApp(firebaseConfig);
+ // window.db = firebase.firestore();
+ // window.auth = firebase.auth();
+//</script>
+
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "YOUR_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyCCiWI6isg8VwVE9cNJKVByF_dffwsua8A",
+    authDomain: "survey-app-70556.firebaseapp.com",
+    projectId: "survey-app-70556",
+    storageBucket: "survey-app-70556.firebasestorage.app",
+    messagingSenderId: "206150943819",
+    appId: "1:206150943819:web:337a837e190aaa8d17f9ce",
+    measurementId: "G-FGH68B811W"
   };
-  firebase.initializeApp(firebaseConfig);
-  window.db = firebase.firestore();
-  window.auth = firebase.auth();
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 </script>
