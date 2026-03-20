85bf7d6ac03da26ee995939d7214176a7326b850
KhanithaHn <75299196+KhanithaHn@users.noreply.github.com>
2026-03-20 17:00:23 +0700

    Update firebase.js

--- assets/firebase.js
+++ assets/firebase.js
@@ -40,4 +40,8 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
+
+  firebase.initializeApp(firebaseConfig);
+  window.db = firebase.firestore();
+  window.auth = firebase.auth();
 </script>
